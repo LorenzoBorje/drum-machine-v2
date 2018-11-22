9732c30b6e93438756106d1d5704c111ebcb2c6e
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-11-22 20:25:43 +0800

    added tempo adjustment, save file handling

--- public/app.js
+++ public/app.js
@@ -1,9 +1,28 @@
 const beatMeasure = 16;
-const bpm = 110;
-const bpmPerMilli = 15000/bpm;
+let bpm = 110;
+let bpmPerMilli = (15000/bpm);
 const instruments = ['kick', 'snare', 'hihat', 'openhat'];
 let currentMeasure = 0;
 let currentlyPlaying;
+
+const SAVE = {}
+
+function generatePattern() {
+  instruments.forEach(instrument => {
+    let pattern = [];
+    for (let i = 0; i < beatMeasure; i++) {
+      pattern.push(false);
+    };
+    console.log(pattern);
+    SAVE[instrument] = pattern; 
+  });
+}
+
+function handleSave() {
+  let sequence = generatePattern();
+  console.log(SAVE);
+
+}
 
 function resetSelected() {
     $('.row > .pad.selected').removeClass('selected');
@@ -41,6 +60,7 @@ function switchPlayButton() {
 function togglePlay() {
     switchPlayButton();
     if ($('.playbutton').hasClass('hidden')) {
+        console.log(bpmPerMilli);
         currentlyPlaying = setInterval(playSequencer, bpmPerMilli);
     } else {
         clearInterval(currentlyPlaying);
@@ -51,28 +71,60 @@ function handlePlayButton(){
     $('.playbutton, .stopbutton').click(togglePlay);
 }
 
+function playSound(currentInstrument) {
+  $(`audio[data-key="${currentInstrument}"]`)[0].currentTime = 0;
+  $(`audio[data-key="${currentInstrument}"]`)[0].play();
+}
+
 function handleBeatSelection() { 
-    $('.pad').on('click', e => {
-        $(event.currentTarget).toggleClass('selected');
-        let currentInstrument = $(event.currentTarget).attr('data-key');
-        // chaining buttons heightens pitch, odd side-effect
-        // better solutions exist (see: StackOverflow) but for first iteration this is fine
-        if ($(event.currentTarget).hasClass('selected')) {
-            $(`audio[data-key="${currentInstrument}"]`)[0].currentTime = 0;
-           $(`audio[data-key="${currentInstrument}"]`)[0].play();
-        }
-    });
+  $('.pad').on('click', e => {
+    let pad = $(event.currentTarget);
+    let currentInstrument = pad.attr('data-key');
+    let padClasses = pad.attr('class').split(' ');
+    let padIndex = padClasses[1];
+    
+    pad.toggleClass('selected');
+    // chaining buttons heightens pitch, odd side-effect
+    // better solutions exist (see: StackOverflow) but for first iteration this is fine
+    if ($(event.currentTarget).hasClass('selected')) {
+      SAVE[currentInstrument][padIndex] = true;
+      playSound(currentInstrument);
+    } else {
+      SAVE[currentInstrument][padIndex] = false;
+    }
+    console.log(SAVE);
+  });
+}
+
+function handleLabel() {
+  $('.label').on('click', e => {
+    let currentInstrument = $(event.currentTarget).attr('data-key');
+    playSound(currentInstrument);
+  });
+}
+
+function handleTempoSelection() {
+  $('#tempo').on('input', e => {
+    let setTempo = $('#tempo').val();
+    console.log(setTempo)
+    $('.tempo').text(setTempo); 
+    bpm = setTempo;
+    bpmPerMilli = 15000/bpm;
+  })
 }
 
 function generateDrumSequencerGrid() {
     let htmlString = '';
+    let dividers = [3, 7, 11];
     for (let i = 0; i < instruments.length; i++) {
-         htmlString += `<div class="row instrument ${instruments[i]}"><div class="label">${instruments[i]}</div>`;
+         htmlString += `<div class="row instrument ${instruments[i]}"><button class="label divider" data-key="${instruments[i]}">${instruments[i]}</button>`;
         for (let j = 0; j < beatMeasure; j++) {
-            if (j % 4 === 0 && j !== 0) {
-                htmlString += '<div class="divider">•</div>';
+            if (dividers.includes(j)) {
+              htmlString += `<button class="pad ${j} divider" data-key="${instruments[i]}"></button>`;
+            } else {
+              htmlString += `<button class="pad ${j}" data-key="${instruments[i]}"></button>`;
+
             }
-            htmlString += `<button class="pad ${j}" data-key="${instruments[i]}"></button>`;
          }
          htmlString += `</div>`
     }
@@ -86,9 +138,12 @@ function composeSequencer(){
 
 function handleStart() {
     composeSequencer();
+    handleLabel();
+    handleTempoSelection();
     handleBeatSelection();
     handlePlayButton();
     handleReset();
+    handleSave();
 }
 
 $(handleStart);
